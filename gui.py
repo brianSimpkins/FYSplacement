5ad56c166138cffab460b0152b1548c38d6c590f
brianSimpkins <brian.simpkins@gmail.com>
2022-11-25 00:20:22 -0800

    Allowing for reshuffling

--- gui.py
+++ gui.py
@@ -8,7 +8,15 @@ import copy
 
 from os.path import exists
 
-class WorkerThread(qc.QThread):
+class UpdaterThread(qc.QThread):
+    def __init__(self, place_class):
+        qc.QThread.__init__(self)
+        self.place_class = place_class
+    def run(self):
+        self.place_class.place_students()
+
+
+class PlacerThread(qc.QThread):
     def __init__(self, place_class):
         qc.QThread.__init__(self)
         self.place_class = place_class
@@ -22,7 +30,7 @@ class Placement_Gui(qw.QMainWindow):
         self.title = "Pitzer FYS Placement"
         self.left = 100
         self.top = 100
-        self.width = 600
+        self.width = 900
         self.height = 300
         self.initUI()
     
@@ -44,7 +52,7 @@ class Placement_Gui(qw.QMainWindow):
         title.setFont(qg.QFont("Times", 18, 2))
 
         # anndata label
-        form = qw.QLabel("Preference form filepath:")
+        form = qw.QLabel("Input form filepath:")
         form.setFont(qg.QFont("Times", 11))
 
         self.form_box = qw.QLineEdit(self)
@@ -57,34 +65,24 @@ class Placement_Gui(qw.QMainWindow):
         self.output_box = qw.QLineEdit(self)
         self.output_box.setFont(qg.QFont("Times", 11))
 
-        dem1 = qw.QLabel("Demographic rule 1:")
-        dem1.setFont(qg.QFont("Times", 11))
-
-        self.dem1_box = qw.QLineEdit(self)
-        self.dem1_box.setFont(qg.QFont("Times", 11))
-
-        dem2 = qw.QLabel("Demographic rule 2:")
-        dem2.setFont(qg.QFont("Times", 11))
-
-        self.dem2_box = qw.QLineEdit(self)
-        self.dem2_box.setFont(qg.QFont("Times", 11))
-
-        dem3 = qw.QLabel("Demographic rule 3:")
-        dem3.setFont(qg.QFont("Times", 11))
-
-        self.dem3_box = qw.QLineEdit(self)
-        self.dem3_box.setFont(qg.QFont("Times", 11))
-        
-        dem4 = qw.QLabel("Demographic rule 4:")
-        dem4.setFont(qg.QFont("Times", 11))
-
-        self.dem4_box = qw.QLineEdit(self)
-        self.dem4_box.setFont(qg.QFont("Times", 11))
+        self.button_box = qw.QHBoxLayout()
 
         # run button
-        self.run_button = qw.QPushButton("Run")
+        self.run_button = qw.QPushButton("Initial Placement")
         self.run_button.clicked.connect(self.run_button_press)
-        self.run_button.setFont(qg.QFont("Times", 14))
+        self.run_button.setFont(qg.QFont("Times", 12))
+
+        # run button
+        self.update_button = qw.QPushButton("Update Placement")
+        self.update_button.clicked.connect(self.update_button_press)
+        self.update_button.setFont(qg.QFont("Times", 12))
+
+
+        self.button_box.addWidget(self.run_button)
+
+        self.button_box.addSpacing(50)
+
+        self.button_box.addWidget(self.update_button)
 
         # add widgets to the layout
         layout.addWidget(title, 0, 0, 1, 5, qc.Qt.AlignCenter)
@@ -95,19 +93,7 @@ class Placement_Gui(qw.QMainWindow):
         layout.addWidget(output, 2, 0, 1, 3)
         layout.addWidget(self.output_box, 2, 2, 1, 3)
 
-        layout.addWidget(dem1, 3, 0, 1, 3)
-        layout.addWidget(self.dem1_box, 3, 4)
-
-        layout.addWidget(dem2, 4, 0, 1, 3)
-        layout.addWidget(self.dem2_box, 4, 4)
-
-        layout.addWidget(dem3, 5, 0, 1, 3)
-        layout.addWidget(self.dem3_box, 5, 4)
-
-        layout.addWidget(dem4, 6, 0, 1, 3)
-        layout.addWidget(self.dem4_box, 6, 4)
-
-        layout.addWidget(self.run_button, 7, 0, 1, 5, qc.Qt.AlignCenter)
+        layout.addLayout(self.button_box, 7, 0, 1, 5, qc.Qt.AlignCenter)
 
         #layout.setRowStretch(5, 100)
         layout.setHorizontalSpacing(80)
@@ -125,6 +111,25 @@ class Placement_Gui(qw.QMainWindow):
         self.show()
 
 
+    def update_button_press(self):
+        self.newWindow = qw.QMessageBox(self.wid)
+        if self.form_box.text() == "":
+            self.newWindow.setText("Please input a filepath for the placement forms")
+            self.newWindow.exec()
+        elif not exists(self.form_box.text()):
+            self.newWindow.setText("The inputted filepath for the placement forms is invalid")
+            self.newWindow.exec()
+        elif self.output_box.text() == "":
+            self.newWindow.setText("Please input a valid location for the output")
+            self.newWindow.exec()
+        else: 
+            self.newWindow.setText("Running in progress, please wait")
+            placer = pp.Pitzer_Update_Placement(self.form_box.text())
+            self.workerThread = PlacerThread(placer)
+            self.workerThread.finished.connect(self.end_thread)
+            self.workerThread.start()
+            self.newWindow.exec()
+
     def run_button_press(self):
         self.newWindow = qw.QMessageBox(self.wid)
         if self.form_box.text() == "":
@@ -134,12 +139,12 @@ class Placement_Gui(qw.QMainWindow):
             self.newWindow.setText("The inputted filepath for the placement forms is invalid")
             self.newWindow.exec()
         elif self.output_box.text() == "":
-            self.newWindow.setText("Please input a filepath for the placement forms")
+            self.newWindow.setText("Please input a valid location for the output")
             self.newWindow.exec()
         else: 
             self.newWindow.setText("Running in progress, please wait")
             placer = pp.Pitzer_Placement(self.form_box.text())
-            self.workerThread = WorkerThread(placer)
+            self.workerThread = PlacerThread(placer)
             self.workerThread.finished.connect(self.end_thread)
             self.workerThread.start()
             self.newWindow.exec()
--- placement.py
+++ placement.py
@@ -4,8 +4,236 @@ import numpy as np
 import pandas as pd
 import unicodedata as ud
 import math
-from random import sample, shuffle
+from random import randint, shuffle
 from collections import defaultdict, Counter
+
+class Pitzer_Update_Placement:
+
+    def __init__(self, filepath):
+        # Let's get our data
+        self.prev_placements = pd.read_csv(filepath)
+
+        self.keepers = self.prev_placements[self.prev_placements["Keep Placement"] == "x"]
+        self.new_placements = self.prev_placements[self.prev_placements["Keep Placement"] != "x"]
+
+        self.kept_students = self.keepers.shape[0]
+
+        # Build class - preference dictionary
+        self.class_pref = {x: {"1":[], "n1":0, "2":[], "n2":0, "3":[], "n3":0, "4":[], "n4":0, "5":[], "n5":0} for x in self.prev_placements["Assignment"].unique().tolist()}
+
+        for i in range(len(self.new_placements["CX ID"])):
+            # add student id to self.class_pref dict
+            for pref_num in range(1, 6):
+                pref = str(pref_num)
+                self.class_pref[self.new_placements["Preference " + pref].iloc[i]][pref].append(self.new_placements["CX ID"].iloc[i])
+                self.class_pref[self.new_placements["Preference " + pref].iloc[i]]["n" + pref] += 1
+        
+        # Convert to dataframe
+        self.class_pref = pd.DataFrame(self.class_pref).T
+
+        # For each class, find out how many students are in them already, how many are needed
+        self.class_pref["Students Needed"] = self.class_pref.index.map(lambda classname: sum(self.new_placements["Assignment"] == classname))
+
+        self.class_pref = self.class_pref[self.class_pref["Students Needed"] != 0]
+
+        # For each class, determine "popularity", order by increasing popularity / needed_students
+        self.class_pref["top2"] = self.class_pref["n1"] + self.class_pref["n2"]
+        self.class_pref["top3"] = self.class_pref["top2"] + self.class_pref["n3"]
+        self.class_pref["top4"] = self.class_pref["top3"] + self.class_pref["n4"]
+        self.class_pref["top5"] = self.class_pref["top4"] + self.class_pref["n5"]
+
+        # New kind of re-placement, where class_size is determined by needed_students
+        self.student_assignments = {}
+
+        # Old placements
+        self.previous_assignments = dict(zip(self.keepers["CX ID"], zip(self.keepers["Assignment"], self.keepers["Assignment Preference"])))
+
+    def save_assignments(self, filepath):
+        self.student_assignments.update(self.previous_assignments)
+        self.output = self.prev_placements.copy()
+        self.output["Keep Placement"] = self.output["CX ID"].apply(lambda x: "x")
+        self.output["Assignment"] = self.output["CX ID"].apply(lambda x : self.student_assignments[x][0])
+        self.output["Assignment Preference"] = self.output["CX ID"].apply(lambda x : self.student_assignments[x][1])
+        self.output.sort_values(by = ["Assignment"], inplace = True)
+        self.output.to_csv(path_or_buf = (filepath + "/FYS_StudentAssignments.csv"), index = False)
+
+        self.get_stats()
+        self.stats.to_csv(path_or_buf = (filepath + "/FYS_AssignmentStats.csv"), index = False)
+
+    def get_stats(self, col_names = ["Gender", "IPEDS Classification", "First Gen", "Major Interest by Division"]):
+
+        self.stats = pd.DataFrame(list(dict.fromkeys(list(self.output["Assignment"])))).rename(columns = {0:'Class'})
+
+        def get_maj_prop(group):
+            counter = Counter(list(group))
+            majority_element, maj_count = counter.most_common()[0]
+            maj_prop = maj_count / len(list(counter.elements())) * 100
+            return f"{majority_element}: {maj_prop:.1f}%"
+
+        # for each demographic column
+        for col_name in col_names:
+            new_col = self.output.groupby("Assignment")[col_name].agg(lambda x: get_maj_prop(x))
+            self.stats[col_name] = new_col.values
+        
+
+    def place_students(self):
+        min_student_assignments = {}
+        min_student_happiness = 5
+        min_worst_placement = 5
+
+        # iterate 50 times, make sure everyone is happy!
+        for i in range(100):
+
+            student_assigments = self.place_recursive(self.class_pref.copy(), 0)
+            # in case there are no students marked
+            if student_assigments:
+                happiness, worst_placement = self.get_score(student_assigments)
+                if worst_placement < min_worst_placement:
+                    min_student_assignments = student_assigments
+                    min_worst_placement = worst_placement
+                    min_student_happiness = happiness
+                elif worst_placement <= min_worst_placement and happiness <= min_student_happiness:
+                    min_student_assignments = student_assigments
+                    min_worst_placement = worst_placement
+                    min_student_happiness = happiness
+        
+        self.student_assignments = min_student_assignments
+
+        return min_student_happiness, min_worst_placement
+
+    # get a score for how "happy" people are in their classes
+    def get_score(self, student_assignments):
+        total = 0
+        worst_placement = 0
+
+        for (course, pref) in student_assignments.values():
+
+            total += pref
+
+            worst_placement = max(worst_placement, pref)
+        
+        ave_happiness = total / len(student_assignments)
+        return ave_happiness, worst_placement
+
+    # If our ordering of classes means one can't be filled, swap it with the one above it
+    def swap_up(self, df, i1):
+        # swap with above row
+        i2 = i1 - 1
+
+        # swap row and index
+        df = df.iloc[np.r_[0:i2, i1, i2, i1:(len(df)-1)]]
+
+        return df
+
+
+    # If there aren't enough students to fill a class, reorder and try again!
+    def place_recursive(self, class_preferences, depth):
+
+        if depth > 50:
+            return None
+
+        
+        class_preferences["sort_value"] = class_preferences["top5"] / class_preferences["Students Needed"] * randint(90, 100)
+        class_preferences.sort_values(["sort_value"], ascending = True, inplace = True)
+
+        student_assignments = {}
+
+        # Iterate through the classes, starting with the least popular
+        for class_num, curr_class in enumerate(class_preferences.index):
+
+            # Keep track of gender balance in class
+            gender_map = defaultdict(int)
+
+            # Keep track of racial balance in class
+            race_map = defaultdict(int)
+
+            # Determine the class size - less popular classes will have the smaller class sizes
+            remaining_size = class_preferences.loc[curr_class]["Students Needed"]
+
+            # Give open spots to students who requested, giving greater weight to higher preference
+            curr_preference = 1
+            while remaining_size > 0 and curr_preference <= 5:
+
+                # Get all students with the current preference level
+                curr_students = class_preferences.loc[curr_class][str(curr_preference)]
+
+                # Remove students who have already been placed
+                curr_students = [student for student in curr_students if student not in student_assignments]
+
+                # If there are more students at this preference level than we are looking for
+                if len(curr_students) > remaining_size:
+
+                    # Give priority to non-majority genders to help balance class
+                    students_min_gender_min_race = []
+                    students_min_gender_maj_race = []
+                    students_maj_gender_min_race = []
+                    students_maj_gender_maj_race = []
+                    
+                    # Loop through pool
+                    for student in curr_students:
+                        # Determine gender of student
+                        gender = self.new_placements.loc[self.new_placements["CX ID"] == student]["Gender"].values[0]
+                        race = self.new_placements.loc[self.new_placements["CX ID"] == student]["IPEDS Classification"].values[0]
+                        
+                        # Determine majority gender in the class
+                        if len(gender_map.keys()) > 0:
+                            maj_gender = max(gender_map, key = gender_map.get)
+                        else:
+                            maj_gender = ""
+
+                        # Determine majority race in the class
+                        if len(race_map.keys()) > 0:
+                            maj_race = max(race_map, key = race_map.get)
+                        else:
+                            maj_race = ""
+
+                        # Add student to either minority list or majority list
+                        if gender == maj_gender:
+                            if race == maj_race:
+                                students_maj_gender_maj_race.append(student)
+                            else:
+                                students_maj_gender_min_race.append(student)
+                        else:
+                            if race == maj_race:
+                                students_min_gender_maj_race.append(student)
+                            else:
+                                students_min_gender_min_race.append(student)
+                        
+                        # update gender map (fine guess)
+                        gender_map[gender] += 1
+
+                    shuffle(students_min_gender_min_race)
+                    shuffle(students_min_gender_maj_race)
+                    shuffle(students_maj_gender_min_race)
+                    shuffle(students_maj_gender_maj_race)
+                    
+                    # First place the students who aren't of the majority gender, then the rest
+                    curr_students = (students_min_gender_min_race + students_min_gender_maj_race + students_maj_gender_min_race + students_maj_gender_maj_race)[:remaining_size]
+                
+                for student in curr_students:
+                    # Get gender
+                    gender = self.new_placements.loc[self.new_placements["CX ID"] == student]["Gender"].values[0]
+                    # Get race
+                    race = self.new_placements.loc[self.new_placements["CX ID"] == student]["IPEDS Classification"].values[0]
+                    # Increment gender map
+                    gender_map[gender] += 1
+                    # Increment race map
+                    race_map[race] += 1
+
+                    # Assign student
+                    student_assignments[student] = (curr_class, curr_preference)
+                    # Fill one slot
+                    remaining_size -= 1
+                
+                curr_preference += 1
+            
+            # If we went through all preferences and the class still isn't filled, swap the class up and try again!
+            if remaining_size > 0:
+                return self.place_recursive(class_preferences, depth + 1)
+        
+        return student_assignments
+
+
 
 class Pitzer_Placement:
 
@@ -47,16 +275,10 @@ class Pitzer_Placement:
 
         for i in range(len(self.completed_forms["CX ID"])):
             # add student id to self.class_pref dict
-            self.class_pref[self.completed_forms["Preference 1"].iloc[i]]["1"].append(self.completed_forms["CX ID"].iloc[i])
-            self.class_pref[self.completed_forms["Preference 1"].iloc[i]]["n1"] += 1
-            self.class_pref[self.completed_forms["Preference 2"].iloc[i]]["n2"] += 1
-            self.class_pref[self.completed_forms["Preference 2"].iloc[i]]["2"].append(self.completed_forms["CX ID"].iloc[i])
-            self.class_pref[self.completed_forms["Preference 3"].iloc[i]]["n3"] += 1
-            self.class_pref[self.completed_forms["Preference 3"].iloc[i]]["3"].append(self.completed_forms["CX ID"].iloc[i])
-            self.class_pref[self.completed_forms["Preference 4"].iloc[i]]["n4"] += 1
-            self.class_pref[self.completed_forms["Preference 4"].iloc[i]]["4"].append(self.completed_forms["CX ID"].iloc[i])
-            self.class_pref[self.completed_forms["Preference 5"].iloc[i]]["n5"] += 1
-            self.class_pref[self.completed_forms["Preference 5"].iloc[i]]["5"].append(self.completed_forms["CX ID"].iloc[i])
+            for pref_num in range(1, 6):
+                pref = str(pref_num)
+                self.class_pref[self.completed_forms["Preference " + pref].iloc[i]][pref].append(self.completed_forms["CX ID"].iloc[i])
+                self.class_pref[self.completed_forms["Preference " + pref].iloc[i]]["n" + pref] += 1
 
         # Convert to dataframe
         self.class_pref = pd.DataFrame(self.class_pref).T
@@ -68,7 +290,7 @@ class Pitzer_Placement:
         self.class_pref["top4"] = self.class_pref["top3"] + self.class_pref["n4"]
         self.class_pref["top5"] = self.class_pref["top4"] + self.class_pref["n5"]
 
-        self.class_pref = self.class_pref.sort_values(["top5"])
+        self.class_pref = self.class_pref.sort_values(["top5"], ascending = True)
 
 
         # Add placed students to a set that keeps track of students who can't be placed anymore, and their assigned class
@@ -76,10 +298,12 @@ class Pitzer_Placement:
 
     def save_assignments(self, filepath):
         self.output = self.completed_forms.copy()
+        keep_col = self.output["CX ID"].apply(lambda x: "x")
         placement_col = self.output["CX ID"].apply(lambda x : self.student_assignments[x][0])
         pref_col = self.output["CX ID"].apply(lambda x : self.student_assignments[x][1])
         self.output.insert(1, "Assignment Preference", pref_col)
         self.output.insert(1, "Assignment", placement_col)
+        self.output.insert(1, "Keep Placement", keep_col)
         self.output.sort_values(by = ["Assignment"], inplace = True)
         self.output.to_csv(path_or_buf = (filepath + "/FYS_StudentAssignments.csv"), index = False)
 
@@ -260,7 +484,7 @@ class Pitzer_Placement:
         def get_maj_prop(group):
             counter = Counter(list(group))
             majority_element, maj_count = counter.most_common()[0]
-            maj_prop = maj_count / counter.total() * 100
+            maj_prop = maj_count / len(list(counter.elements())) * 100
             return f"{majority_element}: {maj_prop:.1f}%"
 
         # for each demographic column
